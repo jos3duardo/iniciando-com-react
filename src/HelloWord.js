8748a5e5dfc99c21795fbc76a4b05f3ee352aacc
José Eduardo <jos3duadolopes@gmail.com>
2019-11-18 00:58:43 -0400

    first componente hello word whit react

---
+++ src/HelloWord.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class HelloWord extends Component {
+    render() {
+        return(
+            <div className="my-class-reom-react">
+                <h1>Hello Word react</h1>
+            </div>
+        )
+    }
+}
+
+export default HelloWord
